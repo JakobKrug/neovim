3b5db2e9bd157f5c9c39436fe7acc104dcc97060
Jakob Krug <jakobkrug04@gmail.com>
2025-11-04 15:10:11 +0100

    added autopairs, vhdl and markdown language support

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'echasnovski/mini.nvim', -- Note: Corrected namespace from nvim-mini to echasnovski
+    },
+    opts = {
+      -- Enable rendering by default
+      enabled = true,
+      -- Render in normal, command, and terminal modes
+      render_modes = { 'n', 'c', 't' },
+      -- Maximum file size to render (MB)
+      max_file_size = 10.0,
+    },
+    -- Optional: Only load for markdown files
+    ft = { 'markdown' },
+  },
+}
